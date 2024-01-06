50548fb95ee75dc9255073e2f51f1b2fee738361
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-01-06 14:08:31 +0530

    Place Order Transaction

--- src/edu/ijse/mvc/model/OrderModel.java
+++ src/edu/ijse/mvc/model/OrderModel.java
@@ -4,8 +4,11 @@
  */
 package edu.ijse.mvc.model;
 
+import edu.ijse.mvc.db.DBConnection;
 import edu.ijse.mvc.dto.OrderDetailDto;
 import edu.ijse.mvc.dto.OrderDto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 /**
@@ -13,9 +16,74 @@ import java.util.ArrayList;
  * @author anjanathrishakya
  */
 public class OrderModel {
-    
-    public String placeOrder(OrderDto orderDto, ArrayList<OrderDetailDto> orderDetailDtos){
-        return null;
+
+    public String placeOrder(OrderDto orderDto, ArrayList<OrderDetailDto> orderDetailDtos) throws Exception {
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            connection.setAutoCommit(false);
+
+            String orderSql = "INSERT INTO Orders VALUES(?,?,?)";
+            PreparedStatement orderStatement = connection.prepareStatement(orderSql);
+            orderStatement.setString(1, orderDto.getOrderId());
+            orderStatement.setString(2, orderDto.getOrderDate());
+            orderStatement.setString(3, orderDto.getCustId());
+
+            boolean isOrderSaved = orderStatement.executeUpdate() > 0;
+
+            if (isOrderSaved) {
+                boolean isOrderDetailSave = true;
+                String orderDetailSql = "INSERT INTO orderdetail VALUES(?,?,?,?)";
+                for (OrderDetailDto orderDetailDto : orderDetailDtos) {
+                    PreparedStatement orderDetailStatement = connection.prepareStatement(orderDetailSql);
+                    orderDetailStatement.setString(1, orderDto.getOrderId());
+                    orderDetailStatement.setString(2, orderDetailDto.getItemId());
+                    orderDetailStatement.setInt(3, orderDetailDto.getQty());
+                    orderDetailStatement.setDouble(4, orderDetailDto.getDiscount());
+
+                    if (!(orderDetailStatement.executeUpdate() > 0)) {
+                        isOrderDetailSave = false;
+                    }
+                }
+
+                if (isOrderDetailSave) {
+
+                    boolean isItemUpdated = true;
+                    String itemSql = "UPDATE item SET QtyOnHand = QtyOnHand - ? WHERE ItemCode = ?";
+                    for (OrderDetailDto orderDetailDto : orderDetailDtos) {
+                        PreparedStatement itemStetement = connection.prepareStatement(itemSql);
+                        itemStetement.setInt(1, orderDetailDto.getQty());
+                        itemStetement.setString(2, orderDetailDto.getItemId());
+
+                        if (!(itemStetement.executeUpdate() > 0)) {
+                            isItemUpdated = false;
+                        }
+                    }
+                    
+                    if(isItemUpdated){
+                        connection.commit();
+                        return "Success";
+                    } else {
+                        connection.rollback();
+                        return "Item update Error";
+                    }
+
+                } else {
+                    connection.rollback();
+                    return "Order Detail Save Error";
+                }
+
+            } else {
+                connection.rollback();
+                return "Order Save Error";
+            }
+
+        } catch (Exception e) {
+            connection.rollback();
+            e.printStackTrace();
+            return e.getMessage();
+        } finally {
+            connection.setAutoCommit(true);
+        }
     }
-    
+
 }
